5b926ca968907e44cd792f5e7382607b5f23c225
SGAoo7 <j.petter111@gmail.com>
2020-03-28 12:44:02 +0100

    Test.txt

--- Mistiq/src/Mistiq/Application.h
+++ Mistiq/src/Mistiq/Application.h
@@ -6,19 +6,26 @@
 #include "optick.h"
 
 namespace Mistiq{
+	/// @Vbrief Main application class for this application
 	class Application {
 	public:
 		Application();
 		virtual ~Application();
 
+		/// @Vbrief Sets up application
 		void Setup();
+		/// @Vbrief Updates application
 		void Update();
+		/// @Vbrief Cleans the application after closing
 		void Clean();
 
+		/// @Vbrief Returns if the application is open or not
 		bool IsOpen();
 
 	private:
+		/// @Vbrief Unique pointer to the window class of this application
 		std::unique_ptr<Window> m_Window;
+		/// @Vbrief Unique pointer to the GUI manager of this application
 		std::unique_ptr<GUIManager> m_GuiManager;
 	};
 }
